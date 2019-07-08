3e33d8a950a65a3d899e51b6f81466aa39296309
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-08 21:25:54 +0100

    WIP: stretch task, added javascript to change digits to red at 10 second mark

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,6 +1,8 @@
 const timerSecondsSingleDigit = document.querySelector('.digit:nth-of-type(2)')
 const timerSecondsDoubleDigit = document.querySelector('.digit:nth-of-type(1)')
-// console.log(timerSecondsDoubleDigit)
+const timerAll = document.querySelectorAll('.digit')
+
+console.log(timerAll)
 
 let secondTimer = setInterval(countTimer, 1000);
 let secondsCountSingleDigit = 0
@@ -10,12 +12,15 @@ function countTimer() {
     if (secondsCountSingleDigit === 9) {
         timerSecondsSingleDigit.textContent = 0
         timerSecondsDoubleDigit.textContent = 1
+
+        timerAll.forEach(digit => digit.style.color = 'red')
         secondsCountSingleDigit = 0
-        secondsCountDoubleDigit = 0
+        
     } else {
     ++secondsCountSingleDigit
+    timerSecondsDoubleDigit.textContent = 0
+    timerAll.forEach(digit => digit.style.color = 'black')
     timerSecondsSingleDigit.textContent = secondsCountSingleDigit
     timerSecondsDoubleDigit.textContent = secondsCountDoubleDigit
-    // console.log(secondsCount)
     }
 }
