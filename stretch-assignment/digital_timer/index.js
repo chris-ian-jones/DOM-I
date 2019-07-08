9cd2bc5c3813c24c422c87d539df1b9b0fc97b46
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-08 21:12:01 +0100

    WIP: stretch task, added javascript to count up to 10 seconds only

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,11 +1,21 @@
 const timerSecondsSingleDigit = document.querySelector('.digit:nth-of-type(2)')
-console.log(timerSecondsSingleDigit)
+const timerSecondsDoubleDigit = document.querySelector('.digit:nth-of-type(1)')
+// console.log(timerSecondsDoubleDigit)
 
-let timer = setInterval(countTimer, 1000);
-let secondsCount = 0
+let secondTimer = setInterval(countTimer, 1000);
+let secondsCountSingleDigit = 0
+let secondsCountDoubleDigit = 0
 
 function countTimer() {
-    ++secondsCount
-    timerSecondsSingleDigit.textContent = secondsCount
-    console.log(secondsCount)
+    if (secondsCountSingleDigit === 9) {
+        timerSecondsSingleDigit.textContent = 0
+        timerSecondsDoubleDigit.textContent = 1
+        secondsCountSingleDigit = 0
+        secondsCountDoubleDigit = 0
+    } else {
+    ++secondsCountSingleDigit
+    timerSecondsSingleDigit.textContent = secondsCountSingleDigit
+    timerSecondsDoubleDigit.textContent = secondsCountDoubleDigit
+    // console.log(secondsCount)
+    }
 }
